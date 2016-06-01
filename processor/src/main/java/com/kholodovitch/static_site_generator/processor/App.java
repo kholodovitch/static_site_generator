bf538ab9e49e7887f21616da82106fdc0a1b1c35
Alexander Kholodovitch <kholodovitch@gmail.com>
2016-06-01 18:30:17 +0300

    Empty project for main processor

---
+++ processor/src/main/java/com/kholodovitch/static_site_generator/processor/App.java
@@ -0,0 +1,11 @@
+package com.kholodovitch.static_site_generator.processor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
