9527eef8d08e33b2a9e73c2d9efd03ad5b807953
Alexander Kholodovitch <kholodovitch@gmail.com>
2016-06-01 19:11:00 +0300

    Add parsing main arguments

--- processor/src/main/java/com/kholodovitch/static_site_generator/processor/App.java
+++ processor/src/main/java/com/kholodovitch/static_site_generator/processor/App.java
@@ -1,11 +1,44 @@
 package com.kholodovitch.static_site_generator.processor;
 
-/**
- * Hello world!
- *
- */
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	private static Options options;
+
+	private static String inputFolder;
+	private static String outputFolder;
+
+	static {
+		options = new Options();
+		options.addOption("i", true, "input folder");
+		options.addOption("o", true, "output folder");
+	}
+
+	public static void main(String[] args) throws ParseException {
+		if (args.length == 0)
+			help();
+		
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse(options, args);
+		parseOptions(cmd);
+
+		Processor proc = new Processor();
+		proc.start(inputFolder, outputFolder);
+	}
+
+	private static void parseOptions(CommandLine cmd) {
+		inputFolder = cmd.getOptionValue("i");
+		outputFolder = cmd.getOptionValue("o", "build");
+	}
+
+	private static void help() {
+		HelpFormatter formater = new HelpFormatter();
+		formater.printHelp("App", options);
+		System.exit(0);
 	}
 }
---
+++ processor/src/main/java/com/kholodovitch/static_site_generator/processor/Processor.java
@@ -0,0 +1,10 @@
+package com.kholodovitch.static_site_generator.processor;
+
+public class Processor {
+
+	public void start(String inputFolder, String outputFolder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
